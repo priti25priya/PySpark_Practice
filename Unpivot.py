be83deb92dbba0d0000a732c4bb45f5c8c8f97ee
Priti Priya <priti.priya@diggibyte.com>
2024-10-24 18:48:07 +0530

    unpivoting the column

---
+++ Unpivot.py
@@ -0,0 +1,13 @@
+from pyspark.python.pyspark.shell import spark
+
+data={('IT', 8, 5),
+      ('Payroll', 3, 2),
+      ('HR', 2, 4)
+      }
+schema=['dep','male','female']
+df=spark.createDataFrame(data,schema)
+df.show()
+
+from pyspark.sql.functions import expr
+unpivotDf = df.select('dep', expr("stack(2, 'male', male,'female',female) as (gender,ct)"))
+unpivotDf.show()
